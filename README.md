bad419df2972bb85d1a12f86bac65a1e28be6505
francesco <info.francescobianco@gmail.com>
2025-03-28 16:10:34 +0100

    New release!

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hackathon
+
+[![Install Mush](https://github.com/javanile/hackathon/actions/workflows/project@francescobianco@matrix-theme.yml/badge.svg)](https://github.com/javanile/hackathon/actions/workflows/project@francescobianco@matrix-theme.yml)
--- scripts/generate-leaderboard.sh
+++ scripts/generate-leaderboard.sh
@@ -4,15 +4,22 @@ GITHUB_USER="javanile"
 REPO_NAME="mush-get-started"
 TOKEN="${GITHUB_TOKEN}"
 
-FORKS=$(curl -s -H "Authorization: token $TOKEN" "https://api.github.com/repos/$GITHUB_USER/$REPO_NAME/forks" | jq -r '.[].full_name')
+FORKS=$(curl -s -H "Authorization: token $TOKEN" "https://api.github.com/repos/$GITHUB_USER/$REPO_NAME/forks" | jq -r '.[] | {full_name, stargazers_count}')
 
+rm -f data/leaderboard.*
 echo "Fork trovati per $GITHUB_USER/$REPO_NAME:"
-for FORK in $FORKS; do
-    echo "Processing fork: $FORK"
-    # Puoi eseguire operazioni su ogni fork, ad esempio clonarlo:
-    # git clone "https://github.com/$FORK.git"
-    WORKFLOW=".github/workflows/project@${FORK//\//@}.yml"
+for FORK in $(echo "$FORKS" | jq -r '.full_name'); do
+    STARS=$(echo "$FORKS" | jq -r "select(.full_name == \"$FORK\") | .stargazers_count")
+    echo "Processing fork: $FORK, Stars: $STARS"
+
+    WORKFLOW=".github/workflows/@${FORK//\//@}.yml"
     cp .github/workflows/template.yml "${WORKFLOW}"
+    sed -i "s|name: template|name: ${FORK}|g" "${WORKFLOW}"
     sed -i "s|PACKAGE_NAME: console|PACKAGE_NAME: ${FORK#*/}|g" "${WORKFLOW}"
+    echo "${STARS} ${FORK}" >> data/leaderboard.txt
 done
 
+echo "[" > data/leaderboard.json
+sort -nr data/leaderboard.txt | awk '{print "{\"rank\":" NR ", \"project\":\"" $2 "\", \"stars\":" $1 "},"}' >> data/leaderboard.json
+echo "{}" >> data/leaderboard.json
+echo "]" >> data/leaderboard.json
