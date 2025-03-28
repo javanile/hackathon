900feb5803c8590b7e3a78a9c95f6821e5269304
francesco <info.francescobianco@gmail.com>
2025-03-28 15:20:17 +0100

    New release!

--- scripts/generate-leaderboard.sh
+++ scripts/generate-leaderboard.sh
@@ -4,15 +4,15 @@ GITHUB_USER="javanile"
 REPO_NAME="mush-get-started"
 TOKEN="${GITHUB_TOKEN}"
 
-
 FORKS=$(curl -s -H "Authorization: token $TOKEN" "https://api.github.com/repos/$GITHUB_USER/$REPO_NAME/forks" | jq -r '.[].full_name')
-
 
 echo "Fork trovati per $GITHUB_USER/$REPO_NAME:"
 for FORK in $FORKS; do
     echo "Processing fork: $FORK"
     # Puoi eseguire operazioni su ogni fork, ad esempio clonarlo:
     # git clone "https://github.com/$FORK.git"
-    cp .github/workflows/template.yml .github/workflows/project@${FORK//\//@}.yml
+    WORKFLOW=".github/workflows/project@${FORK//\//@}.yml"
+    cp .github/workflows/template.yml "${WORKFLOW}"
+    sed -i "s|PACKAGE_NAME: console|PACKAGE_NAME: ${FORK#*/}|g" "${WORKFLOW}"
 done
 
